4619e4d74975b1149e5885adccb0fcb2a634216b
Asfee Alam <95925137+asfeealam@users.noreply.github.com>
2026-03-23 18:03:57 +0530

    Add files via upload

---
+++ Scripts/Analysis/Cumulative Analysis.sql
@@ -0,0 +1,15 @@
+-- Calculate the total sales per month and the running total of sales over time.
+SELECT
+	order_month,
+	total_sales,
+	SUM(total_sales) OVER(PARTITION BY order_month ORDER BY order_month) AS running_total_sales,
+	AVG(avg_price) OVER(PARTITION BY order_month ORDER BY order_month) AS moving_average_price
+FROM (
+	SELECT
+		DATETRUNC(MONTH, order_date) AS order_month,
+		SUM(sales_amount) AS total_sales,
+		AVG(price) AS avg_price
+	FROM gold.fact_sales
+	WHERE order_date IS NOT NULL
+	GROUP BY DATETRUNC(MONTH, order_date)
+)t;
